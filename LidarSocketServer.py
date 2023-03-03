b1f1215e594dba8cef6a8983fa2180a4526b190c
Brandon Todd <73208459+brandon-todd@users.noreply.github.com>
2023-03-03 13:34:50 -0700

    Update LidarSocketServer.py

--- LidarSocketServer.py
+++ LidarSocketServer.py
@@ -9,29 +9,38 @@ conf = []
 angle=[]
 distance=[]
 while run:
+  #udp socket connection turtlebot located at 192.168.2.242 ip currently (ip address is on screen off turtlebot)
   serverSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
   serverSocket.bind(('192.168.2.242',12000))
   try:
+    #receive data from the client to establish connection and get ip address of client
     reply,server_address = serverSocket.recvfrom(2048)
+    #turlebot lidar located at /dev/RPLIDAR
     lidar = RPLidar('/dev/RPLIDAR')
+    #clean input and get basic info to show lidar working
     lidar.clean_input()
     info = lidar.get_info()
     print(info)
     health = lidar.get_health()
     print(health)
     lidar.clean_input()
+    #perform scan and iterate through scan data
     for i,scan in enumerate(lidar.iter_scans()):
       if (i> len(scan)-8):
         break
  #struct is ifff (int float float) with confidence, angle (degrees), distance(mm), time (miliseconds)
         data = struct.pack('iffq',scan[i][0],scan[i][1],scan[i][2],round(time.time()*1000))
+    #send data to client
     serverSocket.sendto(data,server_address)
+    #except some lidar issue and print lidar failure 
   except RPLidarException:
     print("LidarFailure")
+    #keyboard interrupt stops the script
   except KeyboardInterrupt:
     pass
     run = False
   finally:
+    #finally disconnect from lidar as well as socket
     lidar.stop()
     lidar.stop_motor()
     lidar.disconnect()
